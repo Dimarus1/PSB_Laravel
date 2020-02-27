0d6bfc9d444326bc342945fa1d854c121be9e388
Your Name <you@example.com>
2020-02-27 15:02:21 +0300

    Old Form

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,32 +7,45 @@
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
 </head>
 <body>
-    
-<form class="form-signin">
-  <div class="text-center mb-4">
-    <img class="mb-4" src="/docs/4.4/assets/brand/bootstrap-solid.svg" alt="" width="72" height="72">
-    <h1 class="h3 mb-3 font-weight-normal">Floating labels</h1>
-    <p>Build form controls with floating labels via the <code>:placeholder-shown</code> pseudo-element. <a href="https://caniuse.com/#feat=css-placeholder-shown">Works in latest Chrome, Safari, and Firefox.</a></p>
+   
+<div class="container">
+   <div class="row">
+     <div class="col-12">
+	   <div class="card mt-5">
+          <div class="card-body">
+		  <img src="logozd.png" class="img-fluid img-thumbnail" alt="zdravkurort">
+            <H5 class="card-title">Введите данные бронирования</H5>		  
+                <form action="pay.php" method="POST">
+				<div class="form-group">
+				<label for="order"></label>
+				<input id="order" type="text" name="orderNumber" class="form-control-lg" placeholder="Номер заказа AA000001" required>
+				<small id="emailHelp" class="form-text text-muted"><b class="text-danger">Внимание! Оплачивайте заказ только после подтверждения!</b></small>
+				</div>
+				<div class="form-group">
+				<label for="amnt"></label>
+				<input id="amnt" type="number" name="amount" class="form-control-lg" placeholder="цена 10000" min="1" required>
+				</div>
+				<div class="form-group">
+				<label for="email"></label>
+				<input id="email" type="email" name="email" class="form-control-sm" placeholder="адрес E-mail">
+				<small id="emailHelp" class="form-text text-muted">E-mail для получения чека об оплате</small>
+				</div>
+				<div class="form-group">
+				<label for="phone"></label>
+				<input id="phone" type="number" name="phone" class="form-control-sm" placeholder="Номер телефона">
+				<small id="emailHelp" class="form-text text-muted">Укажите номер телефона в формате 79990000001</small>
+				</div>
+				<div class="form-group">				
+				<button class="btn btn-success" type="submit">К оплате</button>
+				</div>
+				</form>
+				<img src="logom.png" class="img-fluid img-thumbnail" alt="zdravkurort">
+			</div>
+		</div>
+		
+	</div>
   </div>
-
-  <div class="form-label-group">
-    <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
-    <label for="inputEmail">Email address</label>
-  </div>
-
-  <div class="form-label-group">
-    <input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
-    <label for="inputPassword">Password</label>
-  </div>
-
-  <div class="checkbox mb-3">
-    <label>
-      <input type="checkbox" value="remember-me"> Remember me
-    </label>
-  </div>
-  <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
-  <p class="mt-5 mb-3 text-muted text-center">&copy; 2017-2019</p>
-</form>
+</div>
 
 
 </body>
