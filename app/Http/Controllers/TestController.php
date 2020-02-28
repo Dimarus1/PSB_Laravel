17b9c868f66f23b2c0023feaf740782eb8ab17f9
Your Name <you@example.com>
2020-02-28 16:19:59 +0300

    Signed-off-by: Your Name <you@example.com>

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -13,26 +13,29 @@ class TestController extends Controller {
         $data['da']=123;
 
         //$ch = curl_init('https://jsonplaceholder.typicode.com/posts' . http_build_query($data));
-        $ch = curl_init('http://zdravpay.ru/test' . http_build_query($data));
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        //curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-        //curl_setopt($ch, CURLOPT_HEADER, false);
+        $url = "http://zdravpay.ru/test";
+
+        $post_data = array (
+            "foo" => "bar",
+            "query" => "Nettuts",
+            "action" => "Submit"
+        );
+        
+        $ch = curl_init();
+        
+        curl_setopt($ch, CURLOPT_URL, $url);
+        
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        // Указываем, что у нас POST запрос
         curl_setopt($ch, CURLOPT_POST, 1);
-        $res = curl_exec($ch);
+        // Добавляем переменные
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $post_data);
+        
+        $output = curl_exec($ch);
+        
         curl_close($ch);
-        $res = json_decode($res,1);
-        if (empty($res)){
-            // Возникла ошибка:
-            //echo $res['errorMessage']; 
-            echo "NOT WORK";
-                                 
-        } else {
-            // Успех:
-            // Тут нужно сохранить ID платежа в своей БД - $res['orderId']
-            dd($res);
-            // Перенаправление клиента на страницу оплаты.
-            header('Location: ' . 'zdravpay.ru', true);
-               }
+        
+        echo $output;
 
     }
 
