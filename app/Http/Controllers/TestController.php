868f564741769d651c4b5024993562b4cdbf14a4
Your Name <you@example.com>
2020-02-28 16:06:35 +0300

    Signed-off-by: Your Name <you@example.com>

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -14,8 +14,8 @@ class TestController extends Controller {
 
         $ch = curl_init('https://jsonplaceholder.typicode.com/posts' . http_build_query($data));
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-        curl_setopt($ch, CURLOPT_HEADER, false);
+        //curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        //curl_setopt($ch, CURLOPT_HEADER, false);
         curl_setopt($ch, CURLOPT_POST, 1);
         $res = curl_exec($ch);
         curl_close($ch);
