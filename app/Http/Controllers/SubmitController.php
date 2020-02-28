9268f303602ffb74a01f6b9a4621581da65206b9
Your Name <you@example.com>
2020-02-28 13:19:41 +0300

    Signed-off-by: Your Name <you@example.com>

--- app/Http/Controllers/SubmitController.php
+++ app/Http/Controllers/SubmitController.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 class SubmitController extends Controller {
     
     public function submit(Request $req) {
-        dd($req->input('orderNumber', 'amount'));
-        dd($req->input('amount'));
+        print($req->input('orderNumber'));
+        print($req->input('amount'));
     }
 }
