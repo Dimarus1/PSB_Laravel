2bdcdd2814ae3db8458ae601aa2f347e9690ee7e
Your Name <you@example.com>
2020-02-28 15:00:51 +0300

    Signed-off-by: Your Name <you@example.com>

--- app/Http/Controllers/SubmitController.php
+++ app/Http/Controllers/SubmitController.php
@@ -49,7 +49,8 @@ $data['p_sign'] = strtoupper(hash_hmac('sha1', $string, pack('H*', $key)));
 //dd($data);
 //
 
-$ch = curl_init('https://jsonplaceholder.typicode.com/posts' . http_build_query($data));
+//$ch = curl_init('https://jsonplaceholder.typicode.com/posts' . http_build_query($data));
+$ch = curl_init('https://jsonplaceholder.typicode.com/posts');
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 curl_setopt($ch, CURLOPT_HEADER, false);
@@ -57,7 +58,7 @@ curl_setopt($ch, CURLOPT_POST, 1);
 $res = curl_exec($ch);
 curl_close($ch);
 $res = json_decode($res,1);
-if (empty($res['orderId'])){
+if (empty($res)){
     // Возникла ошибка:
     //echo $res['errorMessage']; 
 	dd($res);
