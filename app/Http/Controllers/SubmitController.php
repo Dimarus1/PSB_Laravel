75efbcd513c22371654e868b97f12ac8553d6dd3
Your Name <you@example.com>
2020-02-28 14:58:27 +0300

    Signed-off-by: Your Name <you@example.com>

--- app/Http/Controllers/SubmitController.php
+++ app/Http/Controllers/SubmitController.php
@@ -56,11 +56,11 @@ curl_setopt($ch, CURLOPT_HEADER, false);
 curl_setopt($ch, CURLOPT_POST, 1);
 $res = curl_exec($ch);
 curl_close($ch);
-//$res = json_decode($res,1);
+$res = json_decode($res,1);
 if (empty($res['orderId'])){
     // Возникла ошибка:
     //echo $res['errorMessage']; 
-	echo ('NOT WORK');
+	dd($res);
                          
 } else {
     // Успех:
