2de8becdacbc6321f38eda9dae3ce601f25f0a14
Your Name <you@example.com>
2020-02-28 16:07:46 +0300

    Signed-off-by: Your Name <you@example.com>

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -16,7 +16,7 @@ class TestController extends Controller {
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         //curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
         //curl_setopt($ch, CURLOPT_HEADER, false);
-        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_GET, 1);
         $res = curl_exec($ch);
         curl_close($ch);
         $res = json_decode($res,1);
