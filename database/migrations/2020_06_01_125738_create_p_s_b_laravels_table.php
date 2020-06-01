60b04091f97a87e290a0217895edb31829ac1fa6
Your Name <you@example.com>
2020-06-01 16:27:26 +0300

    Signed-off-by: Your Name <you@example.com>

--- database/migrations/2020_06_01_125738_create_p_s_b_laravels_table.php
+++ database/migrations/2020_06_01_125738_create_p_s_b_laravels_table.php
@@ -15,7 +15,30 @@ class CreatePSBLaravelsTable extends Migration
     {
         Schema::create('p_s_b_laravels', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->timestamps();
+            $table->string('AMOUNT');
+            $table->string('CURRENCY');
+            $table->string('ORDER');
+            $table->string('DESC');
+            $table->string('MERCH_NAME');
+            $table->string('MERCHANT');
+            $table->string('TERMINAL');
+            $table->string('EMAIL');
+            $table->string('TRTYPE');
+            $table->string('TIMESTAMP');
+            $table->string('NONCE');
+            $table->string('BACKREF');
+            $table->string('RESULT');
+            $table->string('RC');
+            $table->string('RCTEXT');
+            $table->string('AUTHCODE');
+            $table->string('RRN');
+            $table->string('INT_REF');
+            $table->string('P_SIGN');
+            $table->string('NAME');
+            $table->string('CARD');
+            $table->string('CHANNEL');
+            $table->string('ADDINFO');
+
         });
     }
 
