0b7b411a0f7e750087823899b24298fafcd463e0
Your Name <you@example.com>
2020-06-02 17:03:36 +0300

    Signed-off-by: Your Name <you@example.com>

--- database/migrations/2020_06_01_125738_create_p_s_b_laravels_table.php
+++ database/migrations/2020_06_01_125738_create_p_s_b_laravels_table.php
@@ -15,29 +15,29 @@ class CreatePSBLaravelsTable extends Migration
     {
         Schema::create('p_s_b_laravels', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->text('AMOUNT');
-            $table->text('CURRENCY');
-            $table->text('ORDER');
-            $table->text('DESC');
-            $table->text('MERCH_NAME');
-            $table->text('MERCHANT');
-            $table->text('TERMINAL');
-            $table->text('EMAIL');
-            $table->text('TRTYPE');
-            $table->text('TIMESTAMP');
-            $table->text('NONCE');
-            $table->text('BACKREF');
-            $table->text('RESULT');
-            $table->text('RC');
-            $table->text('RCTEXT');
-            $table->text('AUTHCODE');
-            $table->text('RRN');
-            $table->text('INT_REF');
-            $table->text('P_SIGN');
-            $table->text('NAME');
-            $table->text('CARD');
-            $table->text('CHANNEL');
-            $table->text('ADDINFO');
+            $table->text('AMOUNT')-> nullable(true);
+            $table->text('CURRENCY')-> nullable(true);
+            $table->text('ORDER')-> nullable(true);
+            $table->text('DESC')-> nullable(true);
+            $table->text('MERCH_NAME')-> nullable(true);
+            $table->text('MERCHANT')-> nullable(true);
+            $table->text('TERMINAL')-> nullable(true);
+            $table->text('EMAIL')-> nullable(true);
+            $table->text('TRTYPE')-> nullable(true);
+            $table->text('TIMESTAMP')-> nullable(true);
+            $table->text('NONCE')-> nullable(true);
+            $table->text('BACKREF')-> nullable(true);
+            $table->text('RESULT')-> nullable(true);
+            $table->text('RC')-> nullable(true);
+            $table->text('RCTEXT')-> nullable(true);
+            $table->text('AUTHCODE')-> nullable(true);
+            $table->text('RRN')-> nullable(true);
+            $table->text('INT_REF')-> nullable(true);
+            $table->text('P_SIGN')-> nullable(true);
+            $table->text('NAME')-> nullable(true);
+            $table->text('CARD')-> nullable(true);
+            $table->text('CHANNEL')-> nullable(true);
+            $table->text('ADDINFO')-> nullable(true);
         });
     }
 
