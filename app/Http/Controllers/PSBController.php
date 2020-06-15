309427102be15e1838f1d9d32f3e1be275fc3f45
Your Name <you@example.com>
2020-06-15 16:28:22 +0300

    Signed-off-by: Your Name <you@example.com>

--- app/Http/Controllers/PSBController.php
+++ app/Http/Controllers/PSBController.php
@@ -9,7 +9,7 @@ class PSBController extends Controller {
     
  public function DMcommit (Request $req){
 
-      if (isset($req->input('P_SIGN'))) {
+      if ($req->input('P_SIGN') !== NULL) {
          
          $DBPush = new PSBLaravel();
          
