4506f3bb225afee2b27fd54282d617d82feb39e2
Your Name <you@example.com>
2020-06-15 17:25:18 +0300

    Signed-off-by: Your Name <you@example.com>

--- resources/views/databd.blade.php
+++ resources/views/databd.blade.php
@@ -7,7 +7,13 @@
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
 </head>
 <body>
-
+<H1>Все Записи</H1>
+@foreach($data as $el)
+<div class="alert alert-info">
+<h3>{{ $el->id }}</h3>
+<p>{{ $el->EMAIL }}</p>
+<p><small>{{ $el->created_at }}</small></p>
+@endforeach
 
 
 
