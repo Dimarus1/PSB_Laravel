f0cefd1ff17549b6224b501018b463e0df87e7d2
Your Name <you@example.com>
2020-06-15 16:39:59 +0300

    Signed-off-by: Your Name <you@example.com>

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -15,8 +15,8 @@
           <div class="card-body">
 		  <img src="logozd.png" class="img-fluid img-thumbnail" alt="zdravkurort">
             <H5 class="card-title">Введите данные бронирования</H5>		  
-			<form action="{{ route('test-form') }}" method="post">
-				<!--<form action="/back" method="post">-->
+			<!--<form action="{{ route('test-form') }}" method="post">-->
+				<form action="/back" method="post">
                 @csrf
 				<div class="form-group">
 				<label for="ORDER"></label>
